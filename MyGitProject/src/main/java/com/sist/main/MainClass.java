8c1d4fc8db1a1f4225f5b628af962b3e3e71c271
user <user@DESKTOP-FEA4IDF>
2024-07-26 17:00:32 +0900

    Merge branch 'master' of https://github.com/zebraProject2/zebraProject.git

--- MyGitProject/src/main/java/com/sist/main/MainClass.java
+++ MyGitProject/src/main/java/com/sist/main/MainClass.java
@@ -4,11 +4,13 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
     System.out.println("Hello Git");
     System.out.println("Hello Git");
     System.out.println("나무");
     System.out.println("규혁");
     System.out.println("혜진");
     System.out.println("경주");
+    System.out.println("asdasd");
 	}
 }
