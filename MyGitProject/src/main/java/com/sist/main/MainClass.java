e38d01fc7b094c3581130485fb033145f5755cb9
user <user@DESKTOP-FEA4IDF>
2024-07-26 16:46:39 +0900

    1111

--- MyGitProject/src/main/java/com/sist/main/MainClass.java
+++ MyGitProject/src/main/java/com/sist/main/MainClass.java
@@ -6,8 +6,9 @@ public class MainClass {
 		// TODO Auto-generated method stub
     System.out.println("Hello Git");
     System.out.println("Hello Git");
-    System.out.println("은영");
+    System.out.println("나무");
     System.out.println("규혁");
+    System.out.println("혜진");
 	}
 
 }
